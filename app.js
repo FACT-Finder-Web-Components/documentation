af67a5828e08ef54fb3744ef904d8ef281000d0d
Tobias Armbruster <tobias.armbruster@omikron.net>
2018-02-22 13:09:30 +0100

    use https

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('*', function (req, res) {
     res.sendFile("index.html", {root: "."});
 });
 
-app.listen(3000, function () {
+app.listen(80, function () {
     console.log('Example app listening on port 3000!');
 });
