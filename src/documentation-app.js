5fe5015a3854672f74740673be25f95e36073f6c
Krzysztof Kozłowski <kkozlowski2@objectivity.co.uk>
2018-07-04 22:43:23 +0200

    refresh dependencies, add paper-tabs

--- src/documentation-app.js
+++ src/documentation-app.js
@@ -10,6 +10,7 @@ import '@polymer/app-layout/app-toolbar/app-toolbar.js';
 import '@polymer/app-route/app-location.js';
 import '@polymer/app-route/app-route.js';
 import '@polymer/iron-pages/iron-pages.js';
+import '@polymer/paper-tabs/paper-tabs.js';
 import '@polymer/iron-selector/iron-selector.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
 
