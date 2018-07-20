739585e5916c25de5f77d03cab5a44115d49e6cf
Tobias Armbruster <tobias.armbruster@omikron.net>
2018-07-20 17:32:07 +0200

    read forwarded proto to handle https redirect

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+'use-strict';
+
 var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
 
 app.get('*', function (req, res) {
+    var protocol = req.get("X-Forwarded-Proto");
+    if (protocol !== "https") {
+        res.redirect(301, 'https://' + req.headers.host + req.url);
+        return;
+    }
     res.sendFile("index.html", {root: "."});
 });
 
