0474a840a6209772f6efb233a61355c82868e18d
Tobias Armbruster <tobias.armbruster@omikron.net>
2018-07-24 14:05:34 +0200

    chg service urls

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
   githubDemosBasePath: "https://github.com/FACT-Finder-Web-Components/demos/blob/master/",
-  demosBasePath: "https://web-components.fact-finder.de/demos/current/",
+  demosBasePath: "https://search-web-components.fact-finder.de/demos/current/",
   factFinder: {
-    url: "https://web-components.fact-finder.de/FACT-Finder-7.2",
+    url: "https://search-web-components.fact-finder.de/FACT-Finder-7.2",
     channel: "webc-doku-text",
     version: "7.2"
   },
-  downloadToolUrl: "https://web-components.fact-finder.de/webcomponents-build-tool"
+  downloadToolUrl: "https://search-web-components.fact-finder.de/webcomponents-build-tool"
 }
