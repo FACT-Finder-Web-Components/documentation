8b8eb87e46a45813540cb51c2de1fa0401adb5ce
fabian.prinz <fabian.prinz@omikron.net>
2018-08-29 10:50:16 +0200

    add comment to hard-coded landing page redirect

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.static(__dirname));
 
 
+// this is the old landing page (still showing up in google results)
+// for a smoother UX it gets redirected immediately without an error page
 app.get('/documentation/get-started', (req, res) => {
     res.redirect(301, '/documentation/install-dist');
 });
