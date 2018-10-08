6a615070450f938caab0f8bb29219f7f311c10ec
fabian.prinz <fabian.prinz@omikron.net>
2018-10-08 17:00:17 +0200

    [FFWEB-805] add pages for ff-middleware and ff-multi-attribute-parsing

--- src/data/api.js
+++ src/data/api.js
@@ -118,6 +118,10 @@ const api = {
             title: "Checkout Tracking",
             noDemo: true
         },
+        "ff-middleware": {
+            path: "ff-middleware",
+            title: "Middleware",
+        },
 
 
         // --- obsolete pages ---
@@ -182,6 +186,7 @@ api.moreFeatures = [
     api.pages["ff-carousel"],
     api.pages["ff-tag-cloud"],
     api.pages["ff-template"],
+    api.pages["ff-middleware"],
 ];
 
 export default api;
