cec7f1e318a263273342636856c87f1365ee6a78
Sebastian Damrath <sebastian.damrath@omikron.net>
2018-11-28 11:16:51 +0100

    [#132] add jar and adjust run script

--- run-export.sh
+++ run-export.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-DOCS=markdown/3.0/en
-API=${DOCS}/api
+VERSION1=1.2
+VERSION3=3.0
+DOCS1=markdown/${VERSION1}/en
+DOCS3=markdown/${VERSION3}/en
+API1=${DOCS1}/api
+API3=${DOCS3}/api
 OF=export
 
 if [ ! -d "$OF" ]; then
     mkdir -p "$OF"
 fi
 
-if [ -d "$API" ]; then
-    java -jar ./documentation-export.jar -d "$DOCS" -a "$API" -o "$OF"
+if [ -d "$API1" ] && [ -d "$API3" ]; then
+    java -jar ./documentation-export.jar -d "$DOCS1" -a "$API1" -o "$OF" -v "$VERSION1"
+    java -jar ./documentation-export.jar -d "$DOCS3" -a "$API3" -o "$OF" -v "$VERSION3"
 else
-    echo "markdown directory doesn't exist"
+    echo "markdown directories don't exist"
     exit 1;
 fi
