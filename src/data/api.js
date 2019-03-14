56c616e0ad725033e899d51208855cdb4e2cb7bd
torsten.knauf <torsten.knauf@omikron.net>
2019-03-14 13:36:48 +0100

    #174-demo-targets change version handling to 1.x and 3.x

--- src/data/api.js
+++ src/data/api.js
@@ -1,9 +1,9 @@
-import api30 from './3.0/api'
-import api12 from './1.2/api'
+import api30 from './3.x/api'
+import api12 from './1.x/api'
 
 const api = {
-    '3.0': {...api30},
-    '1.2': {...api12}
+    '3.x': {...api30},
+    '1.x': {...api12}
 };
 
 export default api;
