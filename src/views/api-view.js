56d407f7a461dd966400dd40f57f9b78f829a8d8
a.laurowski <ext-a.laurowski@omikron.net>
2019-11-05 06:39:30 +0100

    FFWEB-1428: EventAggregator links 404  (#217)
    * FFWEB-1428: Fix 404 links on EventAggregator subpage

--- src/views/api-view.js
+++ src/views/api-view.js
@@ -194,9 +194,8 @@ class ApiView extends ViewMixin(ReduxMixin(PolymerElement)) {
             e.preventDefault();
             this.$.api_markdown.markdown = `## API documentation for this component is not yet available.`
         });
-        if (this.subpage === `core-event-aggregator`) {
-            this._addScrollNavigation();
-        }
+
+        this._addScrollNavigation();
     }
 
     /*
@@ -211,7 +210,9 @@ class ApiView extends ViewMixin(ReduxMixin(PolymerElement)) {
     _addScrollNavigation() {
         const markdown = this.$.docs_markdown;
         markdown.addEventListener(`marked-render-complete`, () => {
-            const anchors = Array.from(markdown.getElementsByTagName(`a`));
+            const anchors = Array.from(markdown.getElementsByTagName(`a`))
+                .filter((a) => !a.attributes.href.value.startsWith('/'));
+
             anchors.forEach(anchor => {
                 anchor.addEventListener(`click`, (e) => {
                     e.preventDefault();
